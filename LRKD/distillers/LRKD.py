e46c2f25d848a6143149d05a2ce566e5c0e3d876
xinxiaoxiaomeng <38664225+xinxiaoxiaomeng@users.noreply.github.com>
2023-11-05 14:32:46 +0800

    Update LRKD.py

--- LRKD/distillers/LRKD.py
+++ LRKD/distillers/LRKD.py
@@ -9,19 +9,9 @@ class LRKDLoss(nn.Module):
         super(LRKDLoss, self).__init__()
         self.T = T
 
-
-    def js_div(self, q, p):
-        q = F.softmax(q, dim=1)
-        p = F.softmax(p, dim=1)
-
-        log_mean = ((q + p) / 2).log()
-        loss = (F.kl_div(log_mean, q, reduction='sum') + F.kl_div(log_mean, p, reduction='sum')) / 2
-        return loss
-
     def kl(self, y_s, y_t):
         p_s = F.log_softmax(y_s / self.T, dim=1)
         p_t = F.softmax(y_t / self.T, dim=1)
-        # 计算KL散度，p_t指导p_s，后者指导前者的训练，p_t真实分布，p_s是预测分布取log值
         loss = F.kl_div(p_s, p_t, reduction='sum') * (self.T ** 2) / y_s.shape[0]
         return loss
 
